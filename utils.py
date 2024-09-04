2990bdc48ebe844ad5840f0df1275f08b3a7acec
sagan-hartley <sagan.hartley@gmail.com>
2024-09-04 16:37:53 -0400

    market_close_date

--- utils.py
+++ utils.py
@@ -1,15 +1,14 @@
 import numpy as np
 from datetime import datetime
 
-def get_ZCB_vector(market_close_date, payment_dates, rate_vals, rate_dates):
+def get_ZCB_vector(payment_dates, rate_vals, rate_dates):
     """
     Calculate the discount factors for each payment date given a set of rates and rate dates.
 
     Parameters:
-    market_close_date (datetime): The market close date.
     payment_dates (list of datetime): A list of payment dates.
     rate_vals (list of float): A list of discount rates (as decimals).
-    rate_dates (list of datetime): A list of the corresponding dates to rate_vals.
+    rate_dates (list of datetime): A list of the dates corresponding to rate_vals where the first entry is the market close date.
 
     Returns:
     numpy.ndarray: An array of discount factors for the given payment dates.
@@ -26,30 +25,23 @@ def get_ZCB_vector(market_close_date, payment_dates, rate_vals, rate_dates):
 
         # Iterate over the rate dates
         for j, rate_date in enumerate(rate_dates):
-            # Calculate the time period to use
-            if j == 0:
-                # Special case when j equals 0, the previous rate date is set to the settle date
-                # and the end date is the current rate date
-                prev_date = market_close_date
-                end_date = rate_date
-            elif payment_date < rate_date:
-                # For the end date, use the payment date if it's before the current rate date
+            # Calculate the end date to use
+            if payment_date < rate_date:
+                # Use the payment date if it's before the current rate date
                 end_date = payment_date
             else:
                 # Otherwise, use the rate date
                 end_date = rate_date
             
             # Calculate the time difference in years
-            time_period = (end_date - prev_date).days / 365.0
+            time_period = (end_date - rate_date).days / 365.0
             
             # Update the discount factor
             discount_factor *= np.exp(-rate_vals[j] * time_period)
             
-            # If the end date is the payment date, break out, else update the previous date
+            # If the end date is the payment date, break out
             if end_date == payment_date:
                 break
-            else: 
-                prev_date = rate_date
 
         # Store the discount factor for the current payment date
         discount_factors[i] = discount_factor
